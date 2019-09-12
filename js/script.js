5ae9e792ebf329270ee46d94c6e3e3130e11b9af
Anna Szmyga <anna.szmyga@o2.pl>
2019-09-12 14:42:28 +0200

    Add animation for sections headings

--- js/script.js
+++ js/script.js
@@ -8,3 +8,33 @@ const toggleMenu = () => {
 }
 
 hamburger.addEventListener('click', toggleMenu);
+
+
+// start animation when element is in viewport
+const animatedElements = document.querySelectorAll('.section__title');
+
+const checkIfInViewport = (element, callbackIfYes, callbackIfNot) => {
+    const elementCoordinates = element.getBoundingClientRect();
+    const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+    const elementHeight = element.offsetHeight;
+    if ( (elementCoordinates.top >= 0) && (elementCoordinates.bottom < (viewportHeight + elementHeight)) ) {
+        console.log('in viewport!');
+        callbackIfYes()
+    } else {
+        console.log('not...');
+        callbackIfNot();
+    }
+}
+
+const startAnimation = (element) => {
+    element.classList.add('animated');
+}
+const takeOffAnimation = (element) => {
+    element.classList.remove('animated');
+}
+
+animatedElements.forEach((element) => {
+    window.addEventListener('scroll', () => checkIfInViewport(element, () => startAnimation(element), () => takeOffAnimation(element)));
+})
+
+
